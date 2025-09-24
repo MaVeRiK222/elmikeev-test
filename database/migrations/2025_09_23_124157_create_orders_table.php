d641e64cf8de51340f03aa645f0ed7e94350d336
Alexey <panfilov.a@roistat.com>
2025-09-24 19:28:32 +0300

    added app port to env example, little changes in readme

--- database/migrations/2025_09_23_124157_create_orders_table.php
+++ database/migrations/2025_09_23_124157_create_orders_table.php
@@ -15,24 +15,24 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string("g_number", 50)->nullable(); // "9586989214519846085",
-            $table->date("date")->nullable(); // "2024-10-11 19->nullable(); //45->nullable(); //11",
-            $table->date("last_change_date")->nullable(); // "2024-10-12",
-            $table->string("supplier_article", 50)->nullable(); // "1bdd7286d7410f4c",
-            $table->string("tech_size", 50)->nullable(); // "66e7dff9f98764da",
-            $table->integer("barcode")->nullable(); // 77243558,
-            $table->decimal("total_price", 10, 2)->nullable(); // "1569.5",
-            $table->integer("discount_percent")->nullable(); // 10,
-            $table->string("warehouse_name", 50)->nullable(); // "Казань",
-            $table->string("oblast", 50)->nullable(); // "Красноярский край",
-            $table->integer("income_id")->nullable(); // 23829313,
-            $table->integer("odid")->nullable(); // "0",
-            $table->integer("nm_id")->nullable(); // 165894499,
-            $table->string("subject")->nullable(); // "f694ab2be55693c7",
-            $table->string("category")->nullable(); // "9f463620982b6cc9",
-            $table->string("brand")->nullable(); // "a66c77274e96b48c",
-            $table->boolean("is_cancel")->nullable(); // false,
-            $table->date("cancel_dt")->nullable(); // null
+            $table->string("g_number", 50)->nullable();
+            $table->date("date")->nullable();
+            $table->date("last_change_date")->nullable();
+            $table->string("supplier_article", 50)->nullable();
+            $table->string("tech_size", 50)->nullable();
+            $table->integer("barcode")->nullable();
+            $table->decimal("total_price", 10, 2)->nullable();
+            $table->integer("discount_percent")->nullable();
+            $table->string("warehouse_name", 50)->nullable();
+            $table->string("oblast", 50)->nullable();
+            $table->integer("income_id")->nullable();
+            $table->integer("odid")->nullable();
+            $table->integer("nm_id")->nullable();
+            $table->string("subject")->nullable();
+            $table->string("category")->nullable();
+            $table->string("brand")->nullable();
+            $table->boolean("is_cancel")->nullable();
+            $table->date("cancel_dt")->nullable();
             $table->timestamps();
         });
     }
